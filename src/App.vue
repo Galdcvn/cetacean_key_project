d7923572d924265bc0f83c3f9d287f523f3dcd2d
Caio Galdino <caio.vn.gald@gmail.com>
2024-02-24 21:06:35 -0300

    Implementar elemnetos e estilizacoes iniciais

---
+++ src/App.vue
@@ -0,0 +1,85 @@
+<script setup>
+import Filtros from "./components/filtros/Filtros.vue"
+</script>
+
+<template>
+  <div class="appScreen">
+    <section class="parteCima">
+      <main>
+        <img src="./assets/logo.svg" alt="Logo" class="logo">
+        <input type="text" name="queryBar" class="queryBar">
+      </main>     
+    </section>
+
+    <section class="parteBaixo">
+      <main>
+        <section class="filtros">
+          <Filtros />
+        </section>
+
+        <section class="cards">
+
+        </section>
+      </main>
+    </section>
+  </div>  
+</template>
+
+<style scoped>
+  .appScreen{
+    width: 100vw;
+    height: 768px;
+  }
+
+  .parteCima{
+    height: 263px;
+    background-color: #306F9B;
+
+    display: flex;
+    justify-content: center;
+  }
+  .parteCima main{
+    width: 1200px;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 40px;
+  }
+  .logo{
+    margin-top: 5px;
+  }
+  .queryBar{
+    width: 996px;
+    height: 46px;
+
+    border-radius: 25px;
+    border: none;
+
+    text-align: center;
+    font-size: 20pt;
+    
+    box-shadow: -2px 3px 5px 3px rgba(32,79,113,0.25);
+  }
+
+
+  .parteBaixo{
+    height: 505px;
+
+    display: flex;
+    justify-content: center;
+  }
+  .parteBaixo main{
+    width: 1200px;
+
+    display: flex;
+   justify-content: space-between;
+    gap: 55px;
+  }
+  .filtros{
+    width: 285px;
+  }
+  .cards{
+    width: 860px;
+  }
+</style>
---
+++ src/components/filtros/Filtros.vue
@@ -0,0 +1,109 @@
+<script setup>
+
+</script>
+
+<template>
+    <header class="cabecalho">
+        <p>Filtros</p>
+        <button>Resetar</button>
+    </header>
+    <main class="corpo">
+        <section class="subordemFiltro">
+            <header class="cabecalhoFiltro">
+                <p>Subordem</p>
+                <button>Resetar</button>
+            </header>
+
+            <main>
+                
+            </main>
+        </section>
+
+        <section class="aguaFiltro">
+            <header class="cabecalhoFiltro">
+                <p>Tipo de Água</p>
+                <button>Resetar</button>
+            </header>
+
+            <main>
+
+            </main>
+        </section>
+
+        <section class="barbatanaFiltro">
+            <header class="cabecalhoFiltro">
+                <p>Barbatana Dorsal</p>
+                <button>Resetar</button>
+            </header>
+            <main>
+
+
+            </main>
+        </section>
+
+        <section class="tamanhoFiltro">
+            <header class="cabecalhoFiltro">
+                <p>Tamanho Máximo</p>
+                <button>Resetar</button>
+            </header>
+
+            <main>
+
+            </main>
+        </section>
+
+        <section class="conservacaoFiltro">
+            <header class="cabecalhoFiltro">
+                <p>Estado de Conservação</p>
+                <button>Resetar</button>
+            </header>
+            
+            <main>
+
+            </main>
+        </section>
+    </main>
+</template>
+
+<style scoped>
+p{
+    margin: 0 0 0 5px;
+    height: 25px;
+    font-size: 20px;
+    line-height: 20pt;
+
+    color: #0D2738;
+}
+button{
+    height: 25px;
+    font-size: 15px;
+    
+    background: none;
+    border: none;
+    color: #306F9B;
+}
+button:hover{
+    cursor: pointer;
+}
+
+.cabecalho{
+    height: 25px;
+    margin: 20px 0 15px 0;
+    
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    font-size: 20pt;
+}
+.corpo header{
+    height: 25px;
+    margin-bottom: 10px;
+    background-color: #D9E3ED;
+    border-radius: 5px;
+
+    display: flex;
+    justify-content: space-between; 
+    align-items: center;   
+}
+</style>
